d8e7ff77d97148cf3b184201a76e08b3dc0d99a2
chaitanyabaranwal <chaitanyabaranwal@gmail.com>
2018-06-11 11:29:24 +0530

    Remove image in profile model

--- website/models.py
+++ website/models.py
@@ -113,7 +113,6 @@ class Profile(models.Model):
     phone = models.CharField(max_length = 20, null = True)
     address = models.TextField(null = True)
     created = models.DateTimeField(auto_now_add = True)
-    image = ResizedImageField(size = [400, 400], upload_to = "images/profiles/", blank = True)
     
     class Meta:
         app_label = 'website'
